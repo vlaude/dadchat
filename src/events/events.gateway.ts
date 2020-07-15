a3a990f482829ea80f37448461a6e11b80fba12f
Vincent Laude <vincent.laude2@gmail.com>
2020-07-15 16:56:27 +0200

    Setup socket server

---
+++ src/events/events.gateway.ts
@@ -0,0 +1,35 @@
+import {
+  MessageBody,
+  OnGatewayConnection,
+  OnGatewayDisconnect,
+  SubscribeMessage,
+  WebSocketGateway,
+  WebSocketServer,
+} from '@nestjs/websockets';
+import { Server } from 'socket.io';
+import { Logger } from '@nestjs/common';
+
+@WebSocketGateway(4000, { namespace: 'events' })
+export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
+  @WebSocketServer()
+  server: Server;
+
+  private logger: Logger = new Logger('EventsGateway');
+  private count: number = 0;
+
+  @SubscribeMessage('new-message')
+  handleMessage(@MessageBody() data: string): void {
+    this.logger.log(`New message received : ${data}`);
+    this.server.emit('new-message', data);
+  }
+
+  handleConnection(client: any, ...args: any[]): any {
+    this.count++;
+    this.logger.log(`New connection. Total : ${this.count}`);
+  }
+
+  handleDisconnect(client: any): any {
+    this.count--;
+    this.logger.log(`Someone disconnected.`);
+  }
+}
