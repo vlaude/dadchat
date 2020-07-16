d4c7acb9b53ddc6286a613fd8be9c3df97ef03b8
Vincent Laude <vincent.laude2@gmail.com>
2020-07-16 18:14:34 +0200

    Add random usernames

--- src/events/events.gateway.ts
+++ src/events/events.gateway.ts
@@ -9,6 +9,17 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 import { Logger } from '@nestjs/common';
+import * as rug from 'random-username-generator';
+
+export type User = {
+  socketId: string;
+  username: string;
+};
+
+export type Message = {
+  poster: string;
+  content: string;
+};
 
 @WebSocketGateway({ namespace: 'events' })
 export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
@@ -16,25 +27,34 @@ export class EventsGateway implements OnGatewayConnection, OnGatewayDisconnect {
   server: Server;
 
   private logger: Logger = new Logger('EventsGateway');
-  private messages: string[] = [];
+  private messages: Message[] = [];
   private count: number = 0;
+  private users: User[] = [];
 
   @SubscribeMessage('new-message')
-  handleMessage(@MessageBody() data: string): void {
+  handleMessage(
+    @MessageBody() data: string,
+    @ConnectedSocket() client: Socket,
+  ): void {
     this.logger.log(`New message received : ${data}`);
-    this.server.emit('new-message', data);
-    this.messages.push(data);
+    const poster = this.users.find(u => u.socketId === client.id).username;
+    const newMessage: Message = { poster, content: data };
+    this.server.emit('new-message', newMessage);
+    this.messages.push(newMessage);
   }
 
   handleConnection(@ConnectedSocket() client: Socket): void {
     this.count++;
     this.logger.log(`New connection. Total : ${this.count}`);
     this.server.emit('users-count', this.count);
+    this.users.push({ socketId: client.id, username: rug.generate() });
     client.emit('all-messages', this.messages);
   }
 
-  handleDisconnect(): void {
+  handleDisconnect(@ConnectedSocket() client: Socket): void {
     this.count--;
+    const userIndex = this.users.findIndex(u => u.socketId === client.id);
+    this.users.splice(userIndex, 1);
     this.logger.log(`Someone disconnected.`);
     this.server.emit('users-count', this.count);
   }
